dbd5d25c6694d172d2a962ca6c22b2ffa1b32650
HashimAziz <hashimkayani97@gmail.com>
2021-04-28 09:03:50 +0500

    basic functionality added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h2>Navbar</h2>;
+  const [showLink, setShowLink] = useState(false);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button class="nav-toggle" onClick={() => setShowLink(!showLink)}>
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container">
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialIcons) => {
+            const { id, url, icon } = socialIcons;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
